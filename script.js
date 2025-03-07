138ad9d46d6ca9cba072d1e947cd02c7382eca63
Pontus-segebo <pontus.segebo@gmail.com>
2025-03-07 15:05:09 +0100

    fix: routing problem

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
 
-window.addEventListener("scroll", (event) => {
-    let y = this.scrollY;
-
-    console.log(y);
-}) 
-
 let date = new Date();
 
-
-// change the time stamp
-// console.log(time);
 let timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
 const currentDateTime = date.toLocaleString("se-SV", { hour: "2-digit", minute: "2-digit" });
@@ -18,15 +9,4 @@ window.onload = function () {
     document.getElementById("time").innerHTML = currentDateTime;
 }
 
-function showNav() {
-
-}
-
-// function updateDateTime() {
-//     let time = date.getTime();
-//     const currentDateTime = date.toLocaleString();
-//     document.querySelector('#time').textContent = currentDateTime;
-// }
-
-// setInterval(updateDateTime, 1000);
 
